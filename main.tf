09b92f734efa4613273815c618671d0186582ca9
Rudra <rishurudrasingh@gmail.com>
2023-06-28 13:43:13 +0530

    modifying

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
 resource "aws_instance" "sm_ec2" {
-    ami = var.ami_id
-    instance_type = var.inst_type
-    tags = var.tag_name
+  ami           = var.ami_id
+  instance_type = var.inst_type
+  tags = {
+    Name = var.tag_name
+    Owner : "rudra.singh@cloudeq.com"
+    Purpose : "training"
+  }
 
-    volume_tags = {
-        Name = "rudra"
-        Owner : "rudra.singh@cloudeq.com"
-        Purpose : "training"
-        availability_zone = "us-east-1"
-        size              = 10
-    }
+  volume_tags = {
+    Name = "rudra"
+    Owner : "rudra.singh@cloudeq.com"
+    Purpose : "training"
+    # availability_zone = "us-east-1"
+    # size              = 10
+  }
 
 }
